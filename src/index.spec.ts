f8192f082283504d04e72a9a75c0bccf82a947a1
Sophia <beyondthesophia@gmail.com>
2021-03-27 21:56:32 +0900

    test: add test codes

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,13 @@
-import { hello } from ".";
+import { uncompressReplayData } from ".";
+import * as fs from "fs";
+import path from "path";
 
-describe("yrp-parser-node", () => {
-    it("should print world", () => {
-        expect(hello()).toBe("world");
+describe("yrp-parser-addon", () => {
+    it("should uncompress replay data properly", () => {
+        const originalFileBuffer = fs.readFileSync(path.join(process.cwd(), "./res/yrp-basic.yrp"));
+
+        expect(() => {
+            uncompressReplayData(originalFileBuffer);
+        }).not.toThrow();
     });
 });
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,8 @@
-// @ts-ignore
 import * as fs from "fs";
+import * as path from "path";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const addon = require("bindings")("yrp-replay-addon.node");
-const buffer = fs.readFileSync("../replay-parser/res/yrp-basic.yrp");
+const buffer = fs.readFileSync(path.join(process.cwd(), "./res/yrp-basic.yrp"));
 
 console.log(addon.uncompressReplayData(buffer));
