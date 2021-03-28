fa2621e6b55158c9db11a693fbc9226f27d2c926
Sophia <beyondthesophia@gmail.com>
2021-03-28 20:13:12 +0900

    chore: remove redundant dependency

--- src/test.ts
+++ src/test.ts
@@ -3,10 +3,6 @@ import * as path from "path";
 
 import { Replay } from ".";
 
-import * as SegfaultHandler from "segfault-handler";
-
-SegfaultHandler.registerHandler("crash.log");
-
 (async () => {
     const buffer = fs.readFileSync(path.join(process.cwd(), "./res/yrp-basic.yrp"));
     const replay = await Replay.fromBuffer(buffer);
